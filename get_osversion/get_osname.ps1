9e9e68bc1785d764b89bc1117a756db63c2abd6e
ucthev <ucthakur@live.com>
2015-10-01 14:54:51 +0530

    Create get_osname.ps1
    first draft code

---
+++ get_osversion/get_osname.ps1
@@ -0,0 +1,16 @@
+#!powershell
+# WANT_JSON
+# POWERSHELL_COMMON
+
+# define result variable in JSON format
+$result = New-Object psobject @{
+name = 'undefined'
+changed = $false
+};
+
+# get operating system name
+$osname = (Get-WmiObject -Class win32_operatingSystem).Caption
+$result.name = $osname
+
+# return result back
+Exit-Json $result;
